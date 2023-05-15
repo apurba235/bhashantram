760a11a0413157256e57531c49e86cf771b5da61
apurba <apurba.nag@in.ey.com>
2023-05-15 17:03:05 +0530

    :feat - merged with saquib branch

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:bhashantram/app/common/consts/consts.dart';
 import 'package:bhashantram/app/data/ui_models/menu_model.dart';
+import 'package:bhashantram/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
@@ -16,7 +17,9 @@ class HomeController extends GetxController {
       menuIcon: AssetConsts.camera,
       menuTitle: 'Chat Bot',
       menuSubTitle: 'Ask question to the bot',
-      onTapMenu: (){}
+      onTapMenu: (){
+        Get.toNamed(Routes.CHAT_BOT);
+      }
     ),
   ];
 
